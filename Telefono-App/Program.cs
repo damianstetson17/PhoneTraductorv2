4769c7ab7d0fc855ea8288f5aaa762062233efe9
Damian Stetson <damianstetson17@gmail.com>
2021-05-31 09:45:25 -0300

    console menu implemented

--- Telefono-App/Program.cs
+++ Telefono-App/Program.cs
@@ -9,18 +9,32 @@ namespace Telefono_App
     {
         static void Main(string[] args)
         {
-            // try
-            // {
-            //     CharController controller = new CharController();
-            //     controller.getSecuence("hi!");
-            // }
-            // catch (System.Exception ex)
-            // {
-            //     Console.WriteLine(ex.Message);
-            // }
+            try
+            {
+                CharController controller = new CharController();
+                menu(controller);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message} Exiting...\n");
+            }
+        }
 
-            CharController controller = new CharController();
-            controller.getSecuence("!!!hYY!");
+        static void menu(CharController controller)
+        {
+            string input = "";
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("=============[]=============");
+                Console.WriteLine("Frase a convertir: ");
+                input = Console.ReadLine();
+                if (input == "-q") break;
+                controller.getSecuence(input);
+                Console.WriteLine("Presiona cualquier tecla para continuar");
+                Console.ReadLine();
+            }
+            Console.WriteLine(". . . Bye !");
         }
     }
 }
